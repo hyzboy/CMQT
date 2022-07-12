30a6983eef76b1d6f80f131cbfabbd8e6b15b505
HuYingzhuo(hugo/hyzboy) <hyzboy@outlook.com>
2022-07-12 11:43:33 +0800

    added bit codes of QtVulkanWindow.cpp

--- src/vulkan/QtVulkanWindow.cpp
+++ src/vulkan/QtVulkanWindow.cpp
@@ -1,1 +1,32 @@
 #include<hgl/qt/QtVulkanWindow.h>
+
+namespace hgl
+{
+    bool QtVulkanWindow::event(QEvent* e)
+    {
+        switch(e->type())
+        {
+            case QEvent::UpdateRequest:
+    //            render();
+                break;
+
+            case QEvent::PlatformSurface:
+                break;
+
+
+            default: break;
+        }
+    }
+
+    void QtVulkanWindow::exposeEvent(QExposeEvent*){}
+    void QtVulkanWindow::hideEvent(QHideEvent* ev){}
+
+    void QtVulkanWindow::keyPressEvent(QKeyEvent*){}
+    void QtVulkanWindow::keyReleaseEvent(QKeyEvent*){}
+    void QtVulkanWindow::mouseMoveEvent(QMouseEvent*){}
+    void QtVulkanWindow::mousePressEvent(QMouseEvent*){}
+    void QtVulkanWindow::mouseReleaseEvent(QMouseEvent*){}
+    void QtVulkanWindow::resizeEvent(QResizeEvent*){}
+    void QtVulkanWindow::moveEvent(QMoveEvent*){}
+    void QtVulkanWindow::wheelEvent(QWheelEvent*){}
+}//namespace hgl
